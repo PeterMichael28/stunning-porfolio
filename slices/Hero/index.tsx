fadeab02026d8f51017dabb0b8797dc347e47567
peter michael <enitanpeters28@gmail.com>
2024-01-16 16:29:02 +0100

    gsap installation

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -1,5 +1,8 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { renderLetters } from '@/utils/helpers';
+import { Content } from '@prismicio/client';
+import { SliceComponentProps } from '@prismicio/react';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
 
 /**
  * Props for `Hero`.
@@ -10,16 +13,73 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <h1 className="text-red-500 text-3xl font-bold">{slice.primary.first_name}</h1>
-      <h1>{slice.primary.last_name}</h1>
-      
-    </section>
-  );
+ const component = useRef(null);
+
+ useEffect(() => {
+  let ctx = gsap.context(() => {
+   // create as many GSAP animations and/or ScrollTriggers here as you want...
+   gsap
+    .timeline()
+    .fromTo(
+     '.name-animation',
+     { x: -100, opacity: 0, rotate: -10 },
+     {
+      x: 0,
+      opacity: 1,
+      rotate: 0,
+
+      ease: 'elastic.out(1,0.3)',
+      duration: 1,
+      transformOrigin: 'left top',
+      stagger: { each: 0.1, from: 'random' },
+     },
+    )
+    .fromTo(
+     '.job-title',
+     {
+      y: 20,
+      opacity: 0,
+      scale: 1.2,
+     },
+     {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      scale: 1,
+      ease: 'elastic.out(1,0.3)',
+     },
+    );
+  }, component);
+  return () => ctx.revert(); // cleanup!
+ }, []);
+ return (
+  <section
+   data-slice-type={slice.slice_type}
+   data-slice-variation={slice.variation}
+  >
+   <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
+    <div className="col-start-1 row-start-1">
+     <h1
+      className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+      aria-label={
+       slice.primary.first_name + ' ' + slice.primary.last_name
+      }
+     >
+      <span className="block text-slate-300 ">
+       {renderLetters(slice.primary.first_name, 'first')}
+      </span>
+      <span className="-mt-[.2em] block text-slate-500  ">
+       {renderLetters(slice.primary.last_name, 'last')}
+      </span>
+     </h1>
+
+     <span className="job-title block bg-gradient-to-tr from-yellow-600 via-yellow-200 to-yellow-600 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">
+      {slice.primary.description}
+     </span>
+    </div>
+   </div>
+  </section>
+ );
 };
 
 export default Hero;
---
+++ utils/helpers.tsx
@@ -0,0 +1,14 @@
+import { Content, KeyTextField } from "@prismicio/client";
+
+
+export const renderLetters = ( name: KeyTextField, key: string ) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key}-index inline-block opacity-100 `}
+      >
+        {letter}
+      </span>
+    ));
+  };
