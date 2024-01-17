ffefb38b5b31a3841baf6241e5cacc62e3e825a1
peter michael <enitanpeters28@gmail.com>
2024-01-17 14:26:31 +0100

    section heading

--- slices/Biography/index.tsx
+++ slices/Biography/index.tsx
@@ -1,5 +1,7 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import Bounded from '@/components/Bounded';
+import SectionHeading from '@/components/SectionHeading';
 
 /**
  * Props for `Biography`.
@@ -11,12 +13,17 @@ export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
  */
 const Biography = ({ slice }: BiographyProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+      <SectionHeading size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </SectionHeading>
+      </div>
       Placeholder component for biography (variation: {slice.variation}) Slices
-    </section>
+    </Bounded>
   );
 };
 
