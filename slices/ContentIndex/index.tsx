ba33c293b31c7eb622f107468e536d7207b221a0
peter michael <enitanpeters28@gmail.com>
2024-01-18 16:01:09 +0100

    content List

--- slices/ContentIndex/index.tsx
+++ slices/ContentIndex/index.tsx
@@ -1,23 +1,51 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
 
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { createClient } from "@/prismicio";
+import ContentList from "./ContentList";
+import Bounded from "@/components/Bounded";
+import SectionHeading from "@/components/SectionHeading";
 /**
  * Props for `ContentIndex`.
  */
 export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
+// export type BlogPostIndexProps =
+//   SliceComponentProps<Content.BlogPostIndexSlice>;
 
 /**
  * Component for "ContentIndex" Slices.
  */
-const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+const ContentIndex = async ({ slice }: ContentIndexProps): Promise<JSX.Element> => {
+
+  const client = createClient();
+  const blogPosts = await client.getAllByType("blog_post");
+  const projects = await client.getAllByType("project");
+
+  const items = slice.primary.content_type === "Blog" ? blogPosts : projects;
+
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for content_index (variation: {slice.variation})
-      Slices
-    </section>
+       <SectionHeading size="xl" className="mb-8">
+        {slice.primary.heading}
+      </SectionHeading>
+
+      {isFilled.richText(slice.primary.description) && (
+        <div className="prose prose-xl prose-invert mb-10 text-base">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
+      )}
+
+      <ContentList
+        items={items}
+        contentType={slice.primary.content_type}
+        viewMoreText={slice.primary.view_more_text}
+        fallbackItemImage={slice.primary.fallback_image}
+      />
+    </Bounded>
   );
 };
 
